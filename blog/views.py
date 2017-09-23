602dc406f97f2a97da960ebf81312f609c4be64e
곽하늘 <hannah_k3k@likelion.org>
2017-09-23 15:47:32 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render, get_object_or_404
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post #이 파일이 있는 디렉토리와 같은 디렉토리에 있는 models라서 '.'models
@@ -5,3 +6,7 @@ from .models import Post #이 파일이 있는 디렉토리와 같은 디렉토�
 def post_list(request):
     posts = Post.objects.all().order_by('created_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
