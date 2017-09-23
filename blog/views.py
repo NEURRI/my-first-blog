b26ff12e110cd22cc2858b1d6c7a6de2ae5d6adb
곽하늘 <hannah_k3k@likelion.org>
2017-09-23 15:00:57 +0900

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,5 @@ from django.utils import timezone
 from .models import Post #이 파일이 있는 디렉토리와 같은 디렉토리에 있는 models라서 '.'models
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.all().order_by('created_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
